78df8896f6affbad6cf9932b5683e6790ad67bca
binqisun <30937912+binqisun@users.noreply.github.com>
2017-08-14 14:45:28 -0500

    Add files via upload

---
+++ settings/conf.php
@@ -0,0 +1,111 @@
+<?php 
+
+$language			= 'en';
+
+$template	= 'standard';
+
+$admin_email		= 'admin@localhost';
+
+$print_results		= 1;
+$tmp_dir	= 'tmp';
+
+
+
+$keep_log			= 0;
+
+
+$log_dir	= 'log';
+
+
+$log_format			= 'html';
+
+
+$email_log			= 0;
+
+
+$min_words_per_page = 10;
+
+
+$min_word_length	= 3;
+
+
+$word_upper_bound	= 100;
+
+
+$index_numbers		= 1;
+
+$index_host		 = 0;
+
+
+$index_meta_keywords = 1;
+
+
+$index_pdf	= 0;
+
+
+$index_doc	= 0;
+
+$index_xls	= 0;
+
+$index_ppt	= 0;
+
+$pdftotext_path	= 'c:\temp\pdftotext.exe';
+
+$catdoc_path	= 'c:\temp\catdoc.exe';
+
+$xls2csv_path	= 'c:\temp\xls2csv';
+
+$catppt_path	= 'c:\temp\catppt';
+
+$user_agent			 = 'Sphider';
+
+$min_delay			= 0;
+
+$stem_words			= 0;
+
+
+$strip_sessids			= 1;
+
+
+$results_per_page	= 10;
+
+$cat_columns		= 2;
+
+$bound_search_result = 0;
+
+$length_of_link_desc	= 0;
+
+
+$links_to_next		 = 9;
+$show_meta_description = 1;
+
+$advanced_search	= 0;
+
+$show_query_scores	 = 1;	
+
+$show_categories	 = 1;
+
+$desc_length		= 250;
+
+$merge_site_results		= 0;
+
+$did_you_mean_enabled	= 1;
+
+$suggest_enabled		= 1;
+
+$suggest_history		= 1;
+
+$suggest_keywords		= 0;
+
+$suggest_phrases		= 0;
+
+$suggest_rows		= 10;
+
+
+$title_weight  = 20;
+
+$domain_weight = 60;
+
+$path_weight	= 10;
+
+$meta_weight	= 5;?>
